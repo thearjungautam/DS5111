055541fda88b7495bf80bf2976414b6972390160
thearjungautam <arjungautam2k@gmail.com>
2026-05-27 11:09:48 +0000

    Added initialization scripts

---
+++ scripts/init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sudo apt update -y
+sudo apt install make -y
+sudo apt install python3.14-venv -y
+sudo apt install tree -y
---
+++ scripts/init_git_creds.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+USER="arjungautam2k@gmail.com"
+NAME="thearjungautam"
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name ${NAME}
+
+git config --global --list
